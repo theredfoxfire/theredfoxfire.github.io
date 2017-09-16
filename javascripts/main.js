8d98441a8337c2142c2697ae97a1eeb53b405eed
vizzlearn@gmail.com <vizzlearn@gmail.com>
2017-09-16 23:05:34 +0700

    Understanding PHP Abstract Factory

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,4 @@
-console.log('This would be the main JS file.');
+// console.log('This would be the main JS file.');
+function htmlEntities(str) {
+    return String(str).replace(/&amp;/g, '&').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
